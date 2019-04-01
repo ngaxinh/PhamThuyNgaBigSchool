885728c698a7fb066a44a6b317a2aa10a352afe7
PhamThuyNga <nga120598@gmail.com>
2019-04-02 00:35:22 +0700

    1

--- BigSchool/Controllers/CoursesController.cs
+++ BigSchool/Controllers/CoursesController.cs
@@ -1,5 +1,6 @@
 ﻿using BigSchool.Models;
 using BigSchool.ViewModels;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,13 +18,26 @@ namespace BigSchool.Controllers
         }
         // GET: Courses
         [Authorize]
-        public ActionResult Create()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CourseViewModel viewModel)
         {
-            var viewModel = new CourseViewModel
+            if(!ModelState.IsValid)
             {
-                Categories = _dbContext.Categories.ToList()
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+               
             };
-            return View(viewModel);
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- BigSchool/Controllers/HomeController.cs
+++ BigSchool/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BigSchool.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,10 +8,22 @@ using System.Web.Mvc;
 namespace BigSchool.Controllers
 {
     public class HomeController : Controller
+       
     {
+        private ApplicationDbContext _dbContext;
+        
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upcommingCourses = _dbContext.Courses
+                /*.Include( c => c.Lecturer)
+                .Include( c => c.Category)*/
+                .Where(c => c.DateTime > DateTime.Now);
+            return View(upcommingCourses);
         }
 
         public ActionResult About()
